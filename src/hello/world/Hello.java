54ee4a033fa8e8ddd43eb42dc35b4a2bb3ba7eb3
Beöthy Bence <beothy.bence@gmail.com>
2018-08-03 13:28:25 +0200

    Major bug creation

--- src/hello/world/Hello.java
+++ src/hello/world/Hello.java
@@ -1,9 +1,9 @@
-package hello.world;
+package hello.world
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World")
     }
 
 }
